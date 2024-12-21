5d31c1f344e3cb77f069d7cd8dc5e7806b21e135
SERGIO BRAGA <sergiobg.engineer@gmail.com>
2024-12-21 20:22:10 -0300

    init

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+--
+
+local opts = vim.opt
+opts.clipboard = "unnamedplus"
+
+local opt = vim.opt_global
+opt.scrolloff = 999
+
+vim.g.snacks_animate = false
